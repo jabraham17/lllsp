1ce565babf1d1fa9d03cc2f8068779f64ff3432e
Jade Abraham <jademabraha17@gmail.com>
2024-11-24 12:35:43 -0800

    add prototype extension
    Signed-off-by: Jade Abraham <jademabraha17@gmail.com>

---
+++ editors/vscode/README.md
@@ -0,0 +1,12 @@
+# lllsp Extension
+
+For now, just using [torokati44.glspc]( https://marketplace.visualstudio.com/items?itemName=torokati44.glspc) with the following config, which requires [sunshaoce.llvmir](https://marketplace.visualstudio.com/items?itemName=sunshaoce.llvmir)
+
+```json
+{
+  "glspc.serverPath": "/path/to/lllsp/server/lllsp",
+  "glspc.languageId": "llvm"
+}
+``
+
+This can be setup automatiicaly with `./install.sh`
---
+++ editors/vscode/install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# get the current directory
+FILE_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
+VSCODE_DIR=$(cd $FILE_DIR/../../.vscode ; pwd)
+VSCODE_SETTINGS=$VSCODE_DIR/settings.json
+
+# if the code executable, exists, use it to install the required extensions
+if ! command -v code 2>&1 >/dev/null; then
+  echo "'code' is not installed"
+  exit 1
+fi
+
+echo "Installing 'sunshaoce.llvmir' and 'torokati44.glspc'"
+code --install-extension sunshaoce.llvmir
+code --install-extension torokati44.glspc
+
+echo "Setting up 'glspc' for LLVM"
+
+LLLSP_PATH=$(cd $FILE_DIR/../../server ; pwd)/lllsp
+if [ ! -f $VSCODE_SETTINGS ]; then
+  # if the settings don't already exist, just create them
+  echo "Creating new settings"
+  mkdir -p $VSCODE_DIR
+  touch $VSCODE_SETTINGS
+  echo "{" >> $VSCODE_SETTINGS
+  echo "  \"glspc.serverPath\": \"$LLLSP_PATH\"," >> $VSCODE_SETTINGS
+  echo "  \"glspc.languageId\": \"llvm\"" >> $VSCODE_SETTINGS
+  echo "}" >> $VSCODE_SETTINGS
+else
+
+  if ! command -v jq 2>&1 >/dev/null; then
+    echo "'jq' is not installed, settings will not be updated automatically"
+    exit 0
+  fi
+
+  CUR_SERVER_PATH=$(jq -r '."glspc.serverPath"' $VSCODE_SETTINGS)
+  if [[ "$CUR_SERVER_PATH" != "$LLLSP_PATH" ]]; then
+    echo "Updating 'glspc.serverPath' to '$LLLSP_PATH'"
+    jq ".\"glspc.serverPath\" = \"$LLLSP_PATH\"" $VSCODE_SETTINGS > $VSCODE_SETTINGS.tmp
+    mv $VSCODE_SETTINGS.tmp $VSCODE_SETTINGS
+  fi
+
+  CUR_LANG=$(jq -r '."glspc.languageId"' $VSCODE_SETTINGS)
+  if [[ "$CUR_LANG" != "llvm" ]]; then
+    echo "Updating 'glspc.languageId' to 'llvm'"
+    jq '."glspc.languageId" = "llvm"' $VSCODE_SETTINGS > $VSCODE_SETTINGS.tmp
+    mv $VSCODE_SETTINGS.tmp $VSCODE_SETTINGS
+  fi
+fi
